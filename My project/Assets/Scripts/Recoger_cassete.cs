649ac3382ec2cc8f287f5432a80748fd9ae62fdc
Thelegonic00 <jujupa800@gmail.com>
2025-04-24 19:21:27 -0500

    Todo agregado

--- My project/Assets/Scripts/Recoger_cassete.cs
+++ My project/Assets/Scripts/Recoger_cassete.cs
@@ -3,13 +3,25 @@ using UnityEngine.Video;
 
 public class Recoger_cassete : MonoBehaviour
 {
-    public Movimiento movimientoScript;
+    public GameObject cassete;
+    public GameObject videoCanvas;
     public VideoClip video;
+    public Movimiento movimientoScript;
+    public RenderTexture renderDestino;
+
+    void Start()
+    {
+        videoCanvas.SetActive(false);
+    }
 
     void OnTriggerEnter2D(Collider2D other)
-    {
+    {   
+        
         movimientoScript.Bloqueado = true;
         VideoPlayer vp = gameObject.AddComponent<VideoPlayer>();
+        vp.renderMode = VideoRenderMode.RenderTexture;
+        vp.targetTexture = renderDestino;
+        videoCanvas.SetActive(true);
         vp.clip = video;
         vp.Play();
         vp.loopPointReached += VideoTerminado;
@@ -18,6 +30,8 @@ public class Recoger_cassete : MonoBehaviour
     void VideoTerminado(VideoPlayer vp)
     {
         movimientoScript.Bloqueado = false;
+        videoCanvas.SetActive(false);
+        Destroy(cassete);
         Destroy(this);
     }
 }
